dd91c9b54efa59e3e7df9f3aabf83a541ae33fbb
Arvind-Vishwa <av0216380@gmail.com>
2026-01-10 11:14:13 +0530

    import and export

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ h1.innerHTML="Hello from Arvind"
 
 console.log(h1);
 body.appendChild(h1);
+
+// two types
+// import and export
+// named and default
+// named --> export const arr={1,2,4};  import {arr} from './'
+// default --> export default arr;   import changeArr froom'./'
