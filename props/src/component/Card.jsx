4ef428b838ae36bbae8e8d7bc72a55f64eb3cd4d
Arvind-Vishwa <av0216380@gmail.com>
2026-01-12 14:21:27 +0530

    card props

---
+++ props/src/component/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    
+      <div className="card">
+        <img src='https://images.unsplash.com/photo-1636358860865-3b0ecc67e8c3?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+        <h1>{props.user}</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla ea 
+        Consequuntur, eveniet et. Officiis eaque porro ipsam quod unde voluptatum natus molestiae explicabo voluptas quas ut ipsum dolores ad excepturi, cum error nemo. Delectus possimus similique</p>
+        <button>View Profile</button>
+      </div>
+
+  )
+}
+
+export default Card
