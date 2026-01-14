fd5ab54abdda85e8c9513f3822480d473b36e3fb
Arvind-Vishwa <av0216380@gmail.com>
2026-01-14 16:55:51 +0530

    completed UI

--- ui_project/src/component/Section1/Hero.jsx
+++ ui_project/src/component/Section1/Hero.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const Hero = () => {
-  return (
-    <div className='p-6'>
-        <h3 className='mb-7 text-2xl font-bold'>Prospective<br/> <span>Customer</span><br/>Segmentation</h3>
-        <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing  numquam distinctio, 
-            recusandae cumque saepe explicabo ad unde inventore temporibus praesentium!</p>
-      </div>
-  )
+    return (
+        <div className='p-6'>
+            <h3 className='mb-7 text-7xl font-bold'>Prospective <br /><span >customer</span> <br />segmentation</h3>
+            <p className='text-xl w-[90%] font-medium text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias doloremque perspiciatis cumque, eveniet recusandae a laborum vero! Aperiam, at culpa.</p>
+        </div>
+    )
 }
 
 export default Hero
--- ui_project/src/component/Section1/Section1.jsx
+++ ui_project/src/component/Section1/Section1.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Navbar from './Navbar'
 import Page1Conent from './Page1Conent'
 
-const Section1 = () => {
+const Section1 = (props) => {
   return (
-    <div className='h-screen w-full'>
-      <Navbar/>
-      <Page1Conent />
+    <div className='h-screen w-full '>
+        <Navbar />
+        <Page1Conent users={props.users} />
     </div>
   )
 }
