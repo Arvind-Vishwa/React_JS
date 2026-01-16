1c04d43775bce17095c55ae181c8940c5064e4e0
Arvind-Vishwa <av0216380@gmail.com>
2026-01-16 17:44:04 +0530

    handle form

--- from-handler/src/App.jsx
+++ from-handler/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const App = () => {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("submitted")
+  }
+  const handleChange=(e)=>{
+    console.log(e.target.value)
+  }
   return (
+    
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <input
+        placeholder='Enter name'
+        type='text'
+        
+        onChange={handleChange}
+        />
+        <button >Submit</button>
+      </form>
     </div>
   )
 }
