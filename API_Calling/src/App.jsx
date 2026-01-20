82987c05fbcb09f9f55d9632c033eb293eb6cb3d
Arvind-Vishwa <av0216380@gmail.com>
2026-01-20 18:00:09 +0530

    used a FETCH

--- API_Calling/src/App.jsx
+++ API_Calling/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
 
 const App = () => {
+  
+  const api=async()=>{
+    const data=await fetch('https://jsonplaceholder.typicode.com/users')
+    console.log(data);
+
+    let finalData=await data.json();
+    console.log(finalData);
+  }
+  api();
+
+  const[data,setData]=useState([]);
+
+  
   return (
     <div>
-      API Calling
+      <button >Get a Data</button>
+      {
+
+      }
     </div>
   )
 }
