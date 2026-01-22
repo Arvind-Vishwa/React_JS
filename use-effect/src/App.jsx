d3d352d90d26fe2f505b8f03ff8174a8a9b267ff
Arvind-Vishwa <av0216380@gmail.com>
2026-01-22 15:13:26 +0530

    useffect basics

--- use-effect/src/App.jsx
+++ use-effect/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [num,setNum]=useState(0);
+  const [num2,setNum2]=useState(0);
+
+  useEffect(()=>{
+    console.log("useEffect is running");
+  },[num])
   return (
     <div>
-      App
+      <h1>Value is {num}  and {num2}</h1>
+      <button onClick={()=>{setNum(num+1)}} onDoubleClick={()=>{setNum2(num2+4)}}>Click</button>
     </div>
+    
   )
 }
 
