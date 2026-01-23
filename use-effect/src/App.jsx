23451a33a6b580e9bbe1601aeb273f9a8a87f756
Arvind-Vishwa <av0216380@gmail.com>
2026-01-23 14:47:44 +0530

    update useEffect

--- use-effect/src/App.jsx
+++ use-effect/src/App.jsx
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const[a,setA]=useState(0);
+  const [b,setB]=useState(0);
 
-  const [num,setNum]=useState(0);
-  const [num2,setNum2]=useState(0);
+  function aChange(){
+    console.log("a is changing");
+  }
+  function bChange(){
+    console.log("b is changing");
+  }
 
-  useEffect(()=>{
-    console.log("useEffect is running");
-  },[num])
+  useEffect(function(){
+    
+    aChange();
+},[a]);
+useEffect(function(){
+    
+  bChange();
+},[b])
+  // useEffect(function(){
+  //   console.log("useffect called");
+  // })
+  // if I doen't give [] in useEffect it called again and again chnging in simple state
+
   return (
     <div>
-      <h1>Value is {num}  and {num2}</h1>
-      <button onClick={()=>{setNum(num+1)}} onDoubleClick={()=>{setNum2(num2+4)}}>Click</button>
+      <h1>Value is {a} and {b}</h1>
+      <button onClick={()=>{setA(a+1)}}>+ A</button>
+      <button onClick={()=>{setB(b-1)}}>- B</button>
     </div>
-    
   )
 }
 
